90bddae678723871723b8c538a3594acb2b4ca6d
Сильченко Игорь <Kildsforkids@stud.vvsu.ru>
2020-09-28 16:59:18 +1000

    Добавлена движущаяся точка

---
+++ ConsoleSnake/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Figures
+{
+    class Point
+    {
+        int x;
+        int y;
+        char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Clear()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+        public void Move()
+        {
+            Clear();
+            x++;
+            Draw();
+        }
+    }
+}
---
+++ ConsoleSnake/Program.cs
@@ -0,0 +1,33 @@
+﻿using Figures;
+using System;
+using System.Threading;
+
+namespace ConsoleSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HorizontalLine hLine = new HorizontalLine(0, 0, 16, '#');
+            HorizontalLine hLine2 = new HorizontalLine(0, 10, 16, '#');
+            hLine.Draw();
+            hLine2.Draw();
+
+            VerticalLine vLine = new VerticalLine(0, 0, 10, '#');
+            VerticalLine vLine2 = new VerticalLine(15, 0, 10, '#');
+            vLine.Draw();
+            vLine2.Draw();
+
+            Point p = new Point(2, 3, '*');
+            p.Draw();
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                p.Move();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
